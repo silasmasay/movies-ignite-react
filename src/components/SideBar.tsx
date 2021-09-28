eed7b718d3a9e80fdd3e7ebace25f0680dfcc0ee
silasmasay <silasmasay19@hotmail.com>
2021-09-28 19:26:38 -0300

    Finalização do Desafio

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import './../styles/sidebar.scss';
+
+import { Button } from './Button';
+
+interface GenreResponseProps {
+	id: number;
+	name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	title: string;
+  }
+
+interface SideBar {
+	selectedGenreId: number;
+	genres: GenreResponseProps[];
+	handleClickButton: (id: number) => void;
+} 
+
+export function SideBar({ selectedGenreId, genres, handleClickButton }: SideBar) {
+	return (
+		<nav className="sidebar">
+			<span>Watch<p>Me</p></span>
+
+			<div className="buttons-container">
+			{genres.map(genre => (
+				<Button
+				key={String(genre.id)}
+				title={genre.title}
+				iconName={genre.name}
+				onClick={() => handleClickButton(genre.id)}
+				selected={selectedGenreId === genre.id}
+				/>
+			))}
+			</div>
+		</nav>
+	);
 }
